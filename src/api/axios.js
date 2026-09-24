8661f669499b572de0b0c52fd2217719025610b4
kumarchaman508 <kumarchaman508@gmail.com>
2026-09-25 03:59:00 +0530

    Connect frontend to production backend

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:5000/api",
+    baseURL: "https://interviewace-ai-backend-5jfw.onrender.com/api",
 
     headers: {
         "Content-Type": "application/json",
